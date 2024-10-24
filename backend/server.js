9750fc42f1629d7238ad4cb2531a0d63bdfa2d52
pavan <159290662+paavann@users.noreply.github.com>
2024-10-24 17:53:21 +0530

    fixed errors and conventions

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import Product from "./models/product.model.js";
 dotenv.config();
 
 const app = express();
+app.use(express.json());
 const PORT = 5000;
 
 
@@ -27,16 +28,16 @@ app.post("/products", async (req, res) => {
 
     const user_product = req.body;
 
-    if (!product.name || !product.price || !product.image) {
+    if (!user_product.name || !user_product.price || !user_product.image) {
         res.status(400).json({ success: false, message: "all the fields are mandatory..." });
     }
 
     const newProduct = new Product(user_product); //i just created an instance here.
 
     try {
-        await newProduct.save(); //i'm saving the doc to the db.
-        res.status(200).json({ success: true, data: newProduct });
-    } catch {
+        await newProduct.save(); //saving the doc to the db.
+        res.status(201).json({ success: true, data: newProduct });
+    } catch (error) {
         console.error("error: ", error.message);
         res.status(500).json({ success: false, message: "internal server error" });
     }
